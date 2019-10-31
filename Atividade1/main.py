d334034965581d8982b56b4e2dfc16706ce625ee
Andrew Figueiredo <45156832+Andrew-Figueiredo@users.noreply.github.com>
2019-10-31 20:49:25 -0300

    Concluidoooo!!!!

--- Atividade1/main.py
+++ Atividade1/main.py
@@ -1,20 +1,39 @@
 from teste import test1,test2
 import os
+import sys
 # Pode ser feito alteração no vetor
-vetor = [ 0, 3, 5, 2, 7, 13, 1, 15, 10, 4, 8]
+def readInstance(filePath):
+
+    f = open(filePath,"r")
+    vetor = f.readlines()
+    for i in range(len(vetor)):
+        vetor[i] = int(vetor[i])
+
+    f.close()
+    return vetor
+
+
+
+def main():
+    try:
+        vetor = readInstance(sys.argv[1])
+        os.system('cls') or None
+        print("Qual exemplo voce quer chamar? ")
+        print("1) Heap Máximo. ")
+        print("2) HeapSort ")
+        op = int(input("\nDigite aqui: "))
+        os.system('cls') or None
+        if op == 1 :
+            test1(vetor)
+        elif op == 2 :
+            test2(vetor)
+        else:
+            
+            print("\nOpção Invalida!")
+    except Exception as e:
+        print(e)
+        return
 
 if __name__ == "__main__":
-    os.system('cls') or None
-    print("Qual exemplo voce quer chamar? ")
-    print("1) Heap Máximo. ")
-    print("2) HeapSort ")
-    op = int(input("\nDigite aqui: "))
-    os.system('cls') or None
-    if op == 1 :
-        test1(vetor)
-    elif op == 2 :
-        test2(vetor)
-    else:
-        
-        print("\nOpção Invalida!")
-
+    main()
+    
