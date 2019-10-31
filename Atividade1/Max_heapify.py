7b57253486cdf1ff6d7e0b64c18987ff092391ba
Andrew Figueiredo <andrewdw18@gmail.com>
2019-10-31 06:36:35 -0300

    new files

---
+++ Atividade1/Max_heapify.py
@@ -0,0 +1,41 @@
+def swapPositions(list1, pos1, pos2): 
+    list1[pos1], list1[pos2] = list1[pos2], list1[pos1] 
+    return list1
+def dad(i):
+    return int(i/2)
+
+def left(i):
+    return int(i*2)
+
+def right(i):
+    return int(i*2 + 1)
+
+def max_heapify(A, i,size_heap ):
+    l = left(i)
+    r = right(i)
+    if l<= size_heap and A[l] > A[i] :
+        best = l
+    else:
+        best = i
+    
+    if r <= size_heap and A[r] > A[best] :
+        best = r
+    if best != i :
+        A = swapPositions(A,i,best)
+        max_heapify(A,best,size_heap)
+    return A
+
+def build_max_heap(A):
+    length = len(A)-1
+    size_heap = length
+    for i in range(int(length/2), 0, -1):
+        max_heapify(A,i,size_heap)
+    return length, size_heap
+
+def heapSort(A):
+    length, size_heap = build_max_heap(A)
+    for i in range(length, 0, -1):
+        swapPositions(A,1,i)
+        size_heap -= 1
+        max_heapify(A,1,size_heap)
+    return A
---
+++ Atividade1/main.py
@@ -0,0 +1,12 @@
+from Max_heapify import heapSort,max_heapify,build_max_heap
+   
+A = [ 0,27, 17, 3, 16, 13, 10, 15, 7 , 12, 4, 8, 9, 0]
+#A = [0,4,1,3,2,16,9,10,14,8,7]
+#A = [0,8,9,7,16,10,14]
+print(A)
+#a,b = build_max_heap(A)
+A = heapSort(A)
+# A = max_heapify(A,3)
+
+
+print(A)
